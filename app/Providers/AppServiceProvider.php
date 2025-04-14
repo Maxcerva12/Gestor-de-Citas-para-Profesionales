1826dfcafd598ccba945d8da17d18bf0b437d507
Maxcerva12 <mc349821@gmail.com>
2025-04-14 17:10:42 -0500

    Se agrego el modulo de Full Calendar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+use App\Filament\Widgets\ScheduleCalendarWidget;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Registra manualmente el widget en Livewire
+        Livewire::component('schedule-calendar-widget', ScheduleCalendarWidget::class);
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,11 +43,10 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
-                // \App\Filament\Widgets\ScheduleCalendarWidget::class, // Añade el widget aquí 
             ])
             ->middleware([
                 EncryptCookies::class,
